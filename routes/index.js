1e67f1b5b326e6150cbf020e6f29902144ed1ef2
sfusco <gitlab.com@stevenfusco.com>
2023-02-05 16:18:43 -0600

    adding gitlab snippet support

--- routes/index.js
+++ routes/index.js
@@ -4,19 +4,34 @@ const router = express.Router();
 const nomnoml = require('nomnoml')
 const { Octokit, App } = require('octokit')
 
-async function fetchGist(gist) {
+async function fetchGist(gist_id) {
   try {
     const octokit = new Octokit({
       auth: null
     })
 
     const gist_api_response = await octokit.request('GET /gists/{gist_id}', {
-      gist_id: gist
+      gist_id
     })
 
     console.dir(gist_api_response.data.files)
     const keys = Object.keys(gist_api_response.data.files)
     return gist_api_response.data.files[keys[0]].content
+  } catch (ex) {
+    console.error(ex)
+    return null
+  }
+}
+
+async function fetchSnippet(snippet) {
+  try {
+    const api_root = process.env.GITHUB_API_ROOT || 'https://gitlab.com'
+    const api_response = await fetch(`${api_root}/api/v4/snippets/${snippet}`)
+    const api_data = await api_response.json()
+    console.dir(api_data)
+    const src_url = api_data.files[0].raw_url
+    const src_response = await fetch(src_url)
+    return await src_response.text()
   } catch (ex) {
     console.error(ex)
     return null
@@ -31,11 +46,15 @@ router.get('/', function (req, res, next) {
 router.get('/nn.svg', async (req, res) => {
   let svg = ''
 
-  const gist = req.query.gist
+  const { gist, snippet } = req.query
   let src = req.query.src
 
   if (gist) {
     src = await fetchGist(gist)
+  }
+
+  if (snippet) {
+    src = await fetchSnippet(snippet)
   }
 
   if (src) {
