be3ca412cc0e8108abd81260d3e30805a203c834
sfusco <gitlab.com@stevenfusco.com>
2023-02-05 16:02:19 -0600

    github gist integration

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,47 @@
-var nomnoml = require('nomnoml')
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
+
+const nomnoml = require('nomnoml')
+const { Octokit, App } = require('octokit')
+
+async function fetchGist(gist) {
+  try {
+    const octokit = new Octokit({
+      auth: null
+    })
+
+    const gist_api_response = await octokit.request('GET /gists/{gist_id}', {
+      gist_id: gist
+    })
+
+    console.dir(gist_api_response.data.files)
+    const keys = Object.keys(gist_api_response.data.files)
+    return gist_api_response.data.files[keys[0]].content
+  } catch (ex) {
+    console.error(ex)
+    return null
+  }
+}
 
 /* GET home page. */
 router.get('/', function (req, res, next) {
   res.render('index');
 });
 
-router.get('/nn.svg', (req, res) => {
-  const src = req.query.src
-  console.log(src)
+router.get('/nn.svg', async (req, res) => {
+  let svg = ''
 
-  const svg = nomnoml.renderSvg(src)
+  const gist = req.query.gist
+  let src = req.query.src
+
+  if (gist) {
+    src = await fetchGist(gist)
+  }
+
+  if (src) {
+    console.log(src)
+    svg = nomnoml.renderSvg(src)
+  }
 
   res.status(200)
     .header('Content-Type', 'image/svg+xml')
